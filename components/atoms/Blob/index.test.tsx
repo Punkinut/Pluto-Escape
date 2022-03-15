5322ab97adbed77a0051e68f26f14f43c97179af
Tom Doyle <tomdoyle@Toms-MacBook-Pro.local>
2022-03-15 14:31:15 +1100

    Implemented eslint and prettier settings

--- components/atoms/Blob/index.test.tsx
+++ components/atoms/Blob/index.test.tsx
@@ -1,10 +1,10 @@
-import Blob from ".";
-import { render } from "@testing-library/react";
+import Blob from '.';
+import { render } from '@testing-library/react';
 
-describe("Blob tests", () => {
-  it("Should render", () => {
-    const { container } = render(<Blob />);
+describe('Blob tests', () => {
+    it('Should render', () => {
+        const { container } = render(<Blob />);
 
-    expect(container.innerHTML).toBeDefined();
-  });
+        expect(container.innerHTML).toBeDefined();
+    });
 });
