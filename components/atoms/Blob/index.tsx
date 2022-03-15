49f70a1f470659c7c61418d2f8914b50c1ee2df5
Tom Doyle <tomdoyle@Toms-MacBook-Pro.local>
2022-03-15 16:57:17 +1100

    Removed console.log

--- components/atoms/Blob/index.tsx
+++ components/atoms/Blob/index.tsx
@@ -1,9 +1,7 @@
 import { FC } from 'react';
-import firebase from '../../../firebase/clientApp';
 import styles from './index.module.scss';
 
 const Blob: FC = () => {
-    console.log(firebase);
     return <div className={styles.border}>Pluto Escape</div>;
 };
 
