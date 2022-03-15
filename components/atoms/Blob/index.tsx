d1b265016427aa83bceec992e24f402f06985b21
Tom Doyle <tomdoyle@Toms-MacBook-Pro.local>
2022-03-15 16:50:06 +1100

    Testing firebase on deployment

--- components/atoms/Blob/index.tsx
+++ components/atoms/Blob/index.tsx
@@ -1,7 +1,9 @@
 import { FC } from 'react';
+import firebase from '../../../firebase/clientApp';
 import styles from './index.module.scss';
 
 const Blob: FC = () => {
+    console.log(firebase);
     return <div className={styles.border}>Pluto Escape</div>;
 };
 
