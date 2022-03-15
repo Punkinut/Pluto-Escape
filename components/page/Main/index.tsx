7109c0ec9a558dd9a86611f0e5fbd4764dde1935
Tom Doyle <tomdoyle@Toms-MacBook-Pro.local>
2022-03-15 14:48:17 +1100

    Isolated the page structure a bit

---
+++ components/page/Main/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import Blob from '../../atoms/Blob';
+
+const Main: FC = () => {
+    return (
+        <div>
+            <Blob />
+        </div>
+    );
+};
+
+export default Main;
